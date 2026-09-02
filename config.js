ec54c4bde48190bd1322b05482f395830bef495e
le801030-glitch <le801030@gmail.com>
2026-09-02 10:14:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   APP_NAME: "富林里民即時反映系統",
   LINE_OFFICIAL_URL: "https://lin.ee/YOUR_LINE_LINK",
-  LIFF_ID: "2011368067-R1mD0E34", // 例：1234567890-AbCdEfGh；尚未申請可先留空
+  LIFF_ID: "2011368067-R1mD0E34",
   RESPONSE_PROMISE_DAYS: 7
 };
